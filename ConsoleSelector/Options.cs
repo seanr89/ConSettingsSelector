68fc8b2e9356113467263958c146151f58882cf6
seanr89 <srafferty89@gmail.com>
2023-06-15 21:14:03 +0100

    App Started at least

---
+++ ConsoleSelector/Options.cs
@@ -0,0 +1,7 @@
+
+public record Options
+{
+    public string[] Environments = {"sbx1", "sbx5", "qa", "uat", "prod"};
+    public string[] Projects = {"GMM", "FAB", "WARBA"}; 
+    public string[] Accounts = {"rwrd057", "rwrd058"};
+}
---
+++ ConsoleSelector/Program.cs
@@ -0,0 +1,21 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Write(
+    new FigletText("Console Selector")
+        .LeftJustified()
+        .Color(Color.Red));
+
+var opts = new Options();
+
+// Ask for the user's favorite fruit
+var account = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("Select an [green]Environment[/]?")
+        .PageSize(10)
+        .MoreChoicesText("[grey](Move up and down to reveal more environments)[/]")
+        .AddChoices(opts.Environments));
+
+// Echo the fruit back to the terminal
+AnsiConsole.MarkupLine($"You selected the [red]{account}[/] account!");
+
+AnsiConsole.WriteLine("App Complete!");
