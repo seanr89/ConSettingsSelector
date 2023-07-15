5835ab6e07e131000783084d7f01aadd2780731d
seanr89 <srafferty89@gmail.com>
2023-07-15 23:17:16 +0100

    changes for testing

--- ConsoleSelector/Options.cs
+++ ConsoleSelector/Options.cs
@@ -1,7 +1,23 @@
 
 public record Options
 {
-    public string[] Environments = {"sbx1", "sbx5", "qa", "uat", "prod"};
+    public string[] Regions = {"eu-west-1", "me-central-1"};
+    public string[] Environments = {"dev", "qa", "uat", "prod"};
     public string[] Projects = {"GMM", "FAB", "WARBA"}; 
-    public string[] Accounts = {"rwrd057", "rwrd058"};
+    public string[] Accounts = {"rwrd057", "rwrd058", "rwrd073"};
+    public List<SubOption> AccountSubOptions { get; init; } = new();
+
+    public List<SubOption> CreateAccountSubOptions()
+    {
+        AccountSubOptions.Add(new SubOption {Name = "rwrd057", Values = {"dev", "sbx1", "sbx5"}});
+        AccountSubOptions.Add(new SubOption {Name = "rwrd058", Values = {"qa", "uat", "uat3"}});
+        AccountSubOptions.Add(new SubOption {Name = "rwrd073", Values = {"prod"}});
+        return AccountSubOptions;
+    }
+}
+
+public record SubOption
+{
+    public string Name { get; set; }
+    public List<String> Values { get; set; }
 }
--- ConsoleSelector/Program.cs
+++ ConsoleSelector/Program.cs
@@ -6,16 +6,41 @@ AnsiConsole.Write(
         .Color(Color.Red));
 
 var opts = new Options();
+opts.CreateAccountSubOptions();
 
 // Ask for the user's favorite fruit
 var account = AnsiConsole.Prompt(
     new SelectionPrompt<string>()
+        .Title("Select an [green]Account[/]?")
+        .PageSize(10)
+        .MoreChoicesText("[grey](Move up and down to reveal more accounts)[/]")
+        .AddChoices(opts.Accounts));
+
+// Echo the account back to the terminal
+AnsiConsole.MarkupLine($"You selected the [red]{account}[/] account!");
+
+var env = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
         .Title("Select an [green]Environment[/]?")
         .PageSize(10)
         .MoreChoicesText("[grey](Move up and down to reveal more environments)[/]")
-        .AddChoices(opts.Environments));
+        .AddChoices(opts.AccountSubOptions.First(x => x.Name == account).Values));
 
-// Echo the fruit back to the terminal
-AnsiConsole.MarkupLine($"You selected the [red]{account}[/] account!");
+// Echo the env back to the terminal
+AnsiConsole.MarkupLine($"You selected the [red]{env}[/] environment!");
+
+var project = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("Select a [green]Project[/]?")
+        .PageSize(10)
+        .MoreChoicesText("[grey](Move up and down to reveal more projects)[/]")
+        .AddChoices(opts.Projects));
+
+// Echo the env back to the terminal
+AnsiConsole.MarkupLine($"You selected the [red]{project}[/] project!");
+
+
+AnsiConsole.MarkupLine($"Choices Entered- Account:[red]{account}[/] Env:[red]{env}[/] Project:[red]{project}[/]");
+
 
 AnsiConsole.WriteLine("App Complete!");
