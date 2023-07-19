e4e06c50df4df9a7f2abe7939c7497ac3f18b3e3
seanr89 <srafferty89@gmail.com>
2023-07-19 21:56:20 +0100

    Fixing Acc Creatio

--- ConsoleSelector/Options.cs
+++ ConsoleSelector/Options.cs
@@ -1,18 +1,18 @@
 
-public record Options
+public class Options
 {
     public string[] Regions = {"eu-west-1", "me-central-1"};
     public string[] Environments = {"dev", "qa", "uat", "prod"};
     public string[] Projects = {"GMM", "FAB", "WARBA"}; 
     public string[] Accounts = {"rwrd057", "rwrd058", "rwrd073"};
-    public List<SubOption> AccountSubOptions { get; init; } = new();
+    public List<SubOption> AccountSubOptions { get; set; } = new();
 
-    public List<SubOption> CreateAccountSubOptions()
+    public void CreateAccountSubOptions()
     {
-        AccountSubOptions.Add(new SubOption {Name = "rwrd057", Values = {"dev", "sbx1", "sbx5"}});
-        AccountSubOptions.Add(new SubOption {Name = "rwrd058", Values = {"qa", "uat", "uat3"}});
-        AccountSubOptions.Add(new SubOption {Name = "rwrd073", Values = {"prod"}});
-        return AccountSubOptions;
+        AccountSubOptions = new List<SubOption>();
+        AccountSubOptions.Add(new SubOption() {Name = "rwrd057", Values = new List<string>(new[]{"dev", "sbx1", "sbx5"})});
+        AccountSubOptions.Add(new SubOption() {Name = "rwrd058", Values = new List<string>(new[]{"qa", "uat", "uat3"})});
+        AccountSubOptions.Add(new SubOption() {Name = "rwrd073", Values = new List<string>(new[]{"prod"})});
     }
 }
 
